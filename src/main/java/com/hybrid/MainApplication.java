5b2fdb88613ada935c45105f695b1d21f1ae5076
java <java>
2016-07-20 20:58:12 +0900

    Study2Gradle

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -4,6 +4,7 @@ public class MainApplication {
 	public static void main(String[] args) {
 	 	System.out.println("##################");
 	 	System.out.println("Hello Study2Gradle");
+	 	System.out.println("Hello Study2Gradle");
 	 	System.out.println("##################");
 	}
 }
