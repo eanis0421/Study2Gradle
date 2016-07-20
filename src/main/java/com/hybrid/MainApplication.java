ae7986d64cbf8187a52aacc4ffb781cda1977aae
java <java>
2016-07-20 20:37:58 +0900

    study commit

---
+++ src/main/java/com/hybrid/MainApplication.java
@@ -0,0 +1,9 @@
+package com.hybrid;
+
+public class MainApplication {
+	public static void main(String[] args) {
+	 	System.out.println("##################");
+	 	System.out.println("Hello Study2Gradle");
+	 	System.out.println("##################");
+	}
+}
